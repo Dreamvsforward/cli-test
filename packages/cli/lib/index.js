36e5432aa5a893bfa38383f211c522351f0ae2f0
xiaohua001 <1040152089@.qq.com>
2022-12-02 06:25:17 +0800

    init

---
+++ packages/cli/lib/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+const modulea = require("@xh-cli-test/modulea");
+const moduleb = require("@xh-cli-test/moduleb");
+console.log('init cli')
+console.log(modulea())
+console.log(moduleb())
+
